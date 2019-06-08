42df9b63fc838be940ea6d1c6496a9b3b75c1e6f
explorer-ading <explorer.ading@yahoo.com>
2019-06-08 04:43:29 -0700

    running test layout update.

--- run-test.sh
+++ run-test.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 export LD_LIBRARY_PATH=./doc/lib:$LD_LIBRARY_PATH
 
 #./jsc $@
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+col=10 # change this to whatever column you want the output to start at
+
 for file in ./sunspider/*
 do
-	echo "Running $file " , `./jsc $file`
+	#echo "Running $file , \t\t\t\t\t\ttime(ms):"  `./jsc $file`
+	#printf 'Running %s %s%*s%s\n' "$file" "$GREEN" $col `./jsc $file` "$NORMAL"
+	printf 'Time(ms): %s%*s%s - After running %s \n' "$GREEN" $col `./jsc $file` "$NORMAL"  "$file" 
 done
